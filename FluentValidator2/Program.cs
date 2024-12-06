3c1696685ba75e85130f186840fd73db7c98c6be
tarin <clement.tarin@gmail.com>
2024-12-06 14:48:34 +0100

    Agregar archivos de proyecto.

---
+++ FluentValidator2/Program.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation.Results;
+
+namespace FluentValidator2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var user = new User { Name = "Clement", Age = 15 };
+
+            var validator = new UserValidator();
+            ValidationResult results = validator.Validate(user);
+
+            if (!results.IsValid)
+            {
+                foreach (var error in results.Errors)
+                {
+                    Console.WriteLine($"Property: {error.PropertyName}, Error: {error.ErrorMessage}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("User is valid.");
+            }
+        }
+    }
+}
---
+++ FluentValidator2/UserValidator .cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+namespace FluentValidator2
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            // Set the CascadeMode for this validator
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(user => user.Name)
+                .NotEmpty().WithMessage("Name cannot be empty.")
+                .MinimumLength(3).WithMessage("Name must be at least 3 characters long.");
+
+            RuleFor(user => user.Age)
+                .GreaterThan(0).WithMessage("Age must be greater than 0.")
+                .LessThan(120).WithMessage("Age must be less than 120.");
+        }
+    }
+}
